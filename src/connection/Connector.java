1e337f4dc642504a6ec60ea35392e7805d5c89ae
Bruno Yuji Ishiyama <bruno.yuji.ishiyama@ibm.com>
2018-09-14 21:17:50 -0300

    Server-Cli comms working

---
+++ src/connection/Connector.java
@@ -0,0 +1,67 @@
+package connection;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Connector {
+	public static final int PORT = 9797;
+	private static ServerSocket serverSocket;
+	private static Connector instance;
+
+	private Connector() throws IOException {
+		serverSocket = new ServerSocket(PORT);
+	}
+
+	public static Connector getInstance() throws IOException {
+		if (instance == null)
+			return instance = new Connector();
+		return instance;
+	}
+
+	public void awaitClients() {
+		new Thread(new Runnable() {
+			public void run() {
+				try {
+					while (true) {
+						Socket s = serverSocket.accept();
+						if (s.isConnected()) {
+							System.out.println("Client connected... " + s.getInetAddress() + ":" + s.getPort());
+							new Thread(new Runnable() {
+								public void run() {
+
+									try {
+										manageClient(s);
+									} catch (IOException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									}
+
+								}
+							}).start();
+						}
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+	}
+
+	public void manageClient(Socket s) throws IOException {
+		InputStream input = s.getInputStream();
+		byte[] info = new byte[input.available()];
+		input.read(info);
+		System.out.println(new String(info));
+	}
+
+	public static void main(String[] args) {
+		try {
+			Connector.getInstance().awaitClients();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
