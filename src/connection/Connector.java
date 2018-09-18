2c9868ba6ca8936312f2aff54c74bec2ce0719d5
Bruno Yuji Ishiyama <bruno.yuji.ishiyama@ibm.com>
2018-09-18 18:07:10 -0300

    Server Refactored

--- src/connection/Connector.java
+++ src/connection/Connector.java
@@ -9,7 +9,6 @@ public class Connector {
 	public static final int PORT = 9797;
 	private static ServerSocket serverSocket;
 	private static Connector instance;
-<<<<<<< HEAD
 
 	private Connector() throws IOException {
 		serverSocket = new ServerSocket(PORT);
@@ -21,70 +20,41 @@ public class Connector {
 		return instance;
 	}
 
-=======
-	private Connector() throws IOException {
-		serverSocket = new ServerSocket(PORT);
-	}
-	public static Connector getInstance() throws IOException {
-		if(instance ==  null) return instance = new Connector();
-		return instance;
-	}
->>>>>>> 7183aa3c1ebd1450305256144f9722d6c7073f1e
 	public void awaitClients() {
 		new Thread(new Runnable() {
 			public void run() {
 				try {
 					while (true) {
 						Socket s = serverSocket.accept();
-<<<<<<< HEAD
 						if (s.isConnected()) {
 							System.out.println("Client connected... " + s.getInetAddress() + ":" + s.getPort());
 							new Thread(new Runnable() {
 								public void run() {
 
-=======
-						System.out.println("Client connected... "+s.getInetAddress()+":"+s.getPort());
-						new Thread(new Runnable() {
-							public void run() {
-								while(s.isConnected()) {
->>>>>>> 7183aa3c1ebd1450305256144f9722d6c7073f1e
-									try {
-										manageClient(s);
-									} catch (IOException e) {
-										// TODO Auto-generated catch block
-										e.printStackTrace();
+									while (s.isConnected()) {
+										try {
+											manageClient(s);
+										} catch (IOException e) {
+											// TODO Auto-generated catch block
+											e.printStackTrace();
+										}
 									}
-<<<<<<< HEAD
-
 								}
 							}).start();
 						}
 					}
 				} catch (IOException e) {
-=======
-								}
-							}
-						}).start();
-					}
-				}catch(IOException e) {
->>>>>>> 7183aa3c1ebd1450305256144f9722d6c7073f1e
-					e.printStackTrace();
 				}
 			}
 		}).start();
 	}
-<<<<<<< HEAD
 
 	public void manageClient(Socket s) throws IOException {
-=======
-	public void manageClient(Socket s) throws IOException{
->>>>>>> 7183aa3c1ebd1450305256144f9722d6c7073f1e
 		InputStream input = s.getInputStream();
 		byte[] info = new byte[input.available()];
 		input.read(info);
 		System.out.println(new String(info));
 	}
-<<<<<<< HEAD
 
 	public static void main(String[] args) {
 		try {
@@ -94,6 +64,4 @@ public class Connector {
 			e.printStackTrace();
 		}
 	}
-=======
->>>>>>> 7183aa3c1ebd1450305256144f9722d6c7073f1e
 }
